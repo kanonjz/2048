f84ea858ada6a01c2085b8bbf404901203a35504
kanonjz <540004716@qq.com>
2018-04-11 10:30:16 +0800

    add color to grid

--- game_2048_with_gui.py
+++ game_2048_with_gui.py
@@ -4,17 +4,15 @@ from game_2048_no_gui import *
 SIZE = 500
 GRID_LEN = 4
 GRID_PADDING = 10
-
+FONT = ("Verdana", 40, "bold")
 BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
-BACKGROUND_COLOR_DICT = {   2:"#eee4da", 4:"#ede0c8", 8:"#f2b179", 16:"#f59563", \
-                            32:"#f67c5f", 64:"#f65e3b", 128:"#edcf72", 256:"#edcc61", \
-                            512:"#edc850", 1024:"#edc53f", 2048:"#edc22e" }
+BACKGROUND_COLOR_DICT = { 2:"#eee4da", 4:"#ede0c8", 8:"#f2b179", 16:"#f59563", \
+                          32:"#f67c5f", 64:"#f65e3b", 128:"#edcf72", 256:"#edcc61", \
+                          512:"#edc850", 1024:"#edc53f", 2048:"#edc22e" }
 CELL_COLOR_DICT = { 2:"#776e65", 4:"#776e65", 8:"#f9f6f2", 16:"#f9f6f2", \
                     32:"#f9f6f2", 64:"#f9f6f2", 128:"#f9f6f2", 256:"#f9f6f2", \
                     512:"#f9f6f2", 1024:"#f9f6f2", 2048:"#f9f6f2" }
-FONT = ("Verdana", 40, "bold")
-
 
 
 class Game2048(Frame):
@@ -51,8 +49,9 @@ class Game2048(Frame):
     def updateWidgets(self):
         for i in range(GRID_LEN):
             for j in range(GRID_LEN):
-                if self.matrix[i*4 + j] != 0:
-                    self.grid_matrix[i][j].configure(text=self.matrix[i*4 + j], bg=BACKGROUND_COLOR_CELL_EMPTY)
+                num = self.matrix[i*4 + j]
+                if num != 0:
+                    self.grid_matrix[i][j].configure(text=num, bg=BACKGROUND_COLOR_DICT[num])
                 else:
                     self.grid_matrix[i][j].configure(text="", bg=BACKGROUND_COLOR_CELL_EMPTY)
         self.update_idletasks()
@@ -83,5 +82,5 @@ class Game2048(Frame):
 
 
 if __name__ == '__main__':
-    app = Game2048()
+    game = Game2048()
 
